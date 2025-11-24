093ff374f291bfa294560d636bfb2228fc8bbb72
Peter Barraud <gapeterb@gmail.com>
2025-11-24 22:26:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from mongo import MongoDB, CBPEnv
+from pickle import dump, load
+
+def make_gl_document():
+    gl_doc_file = open('gl.csv', 'w')
+    mdb = MongoDB(CBPEnv.UAT)
+    headerDoc = mdb.HeaderDocument
+    glDoc = mdb.GLDocument
+    # with open('temp.data/header.doc','rb') as f:
+    #     headerDoc = load(f)
+    # with open('temp.data/gl.doc','rb') as f:
+    #     glDoc = load(f)
+    for _, categoryDetails in headerDoc.items():
+        # print(categoryDetails)
+        gl_doc_file.write(f"{categoryDetails['title']},{categoryDetails['type']}\n")
+        for headerDetails in categoryDetails['headers']:
+            categoryID = headerDetails['control']
+            gl_doc_file.write(f"{headerDetails['title']}\n")
+            for gl in glDoc[categoryID]:
+                gl_doc_file.write(f"{gl['view']},{gl['name']},{gl['code']},{gl['isActive']},{gl['isProvisioned']},{gl['type']}\n")
+
+    gl_doc_file.close()
+
+def get_workflow_instances():
+    mdb = MongoDB(CBPEnv.UAT)
+    for wf in mdb.WorkflowInstances:
+        print(wf)
+        break
+
+def get_workflow():
+    mdb = MongoDB(CBPEnv.UAT)
+    query : dict = dict()
+    query['year'] = 2026
+    query['type'] = 'forecast'
+    query['isFinal'] = True
+    for wf in mdb.GetWorkflows(queryParams=query):
+        print(wf['renovated'])
+        break
+def main():
+    get_workflow()
+
+if __name__ == "__main__":
+    main()
+    print("ALL DONE")
---
+++ mongo.py
@@ -0,0 +1,60 @@
+import pymongo
+from enum import Enum
+from json import load
+
+class CBPEnv(Enum):
+    PROD = 1
+    UAT = 2
+
+class MongoDB:
+    def __init__(self, env : CBPEnv):
+        connection_str : str = self.get_connection_string(env)
+        self.mongo_client = pymongo.MongoClient(connection_str)
+        self.mdb = self.mongo_client.get_database("test")
+
+    def get_connection_string(self, env : CBPEnv):
+        f = open('connect.str.json')
+        connection__info = load(f)
+        f.close()
+        return connection__info[env.name]
+
+
+
+    def __del__(self):
+        self.mongo_client.close()
+
+    @property    
+    def Collections(self):
+        """
+            Returns a Cursor of Mongo collections
+            You're going to have to iterate through this to get a specific collection 
+            Example
+            for c in Collections:
+                print(c['name])
+        """
+        return self.mongo_db.list_collections()
+    
+    @property
+    def GLDocument(self):
+        return self.mdb.get_collection('tenant_gls').find().next()['glCodes']
+    
+    @property
+    def HeaderDocument(self):
+        return self.mdb.get_collection('tenant_headers').find().next()['headerCategories']
+    
+    @property
+    def WorkflowInstances(self):
+        """
+        Workflow instance is the complete data for one workflow instance
+        All the data as in all the values for all the individual drivers / gls for all months of that workflow instance
+        """
+        return self.mdb.get_collection('workflowinstances').find()
+    
+    def GetWorkflows(self,**queryParams):
+        """
+        Gets workflows based on the query parameters given
+        """
+        # return self.mdb.get_collection('workflows').find({'year':2024,'type':'forecast','isFinal':True})
+        return self.mdb.get_collection('workflows').find(queryParams['queryParams'])
+    
+    
